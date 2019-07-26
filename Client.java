600f58dc0056dc61d3df558d957600df41c9942f
Siddy234567 <36389835+Siddy234567@users.noreply.github.com>
2019-07-26 19:22:58 -0400

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Client{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+		String input=sc.nextLine();
+}
+
+}
